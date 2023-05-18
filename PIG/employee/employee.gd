59b1adeb47d17aef7003773e7ce9a1d050fa6645
pankogito <103581552+pankogito@users.noreply.github.com>
2023-05-18 20:05:39 +0200

    PIG-59 employee scene set up

---
+++ PIG/employee/employee.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@export_category("Skills")
+@export_range(1,3) var quality = 2 
+@export_range(1,3) var speed = 2 
+@export_range(1,3) var testing = 2 
+
+@export_category("Morale")
+@export var morale = 0
+
+
+var __base_quality
+var __base_speed
+var __base_testing
+var __base_morale
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	__base_quality = quality
+	__base_speed = speed
+	__base_testing = testing
+	
+	__base_morale = morale
+	
+	update_summary()
+
+func update_summary():
+	$Summary.text = name + "\nQ "+ str(quality) + " / S " + str(speed) + " / T " + str(testing) + "\nM "+str(morale)
+	
