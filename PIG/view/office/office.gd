781318bab78579c7e77f099ce9211127d5afafae
PLangowski <pawell092000@gmail.com>
2023-05-28 10:59:16 +0200

    PIG-71: Added the Office script to the scene

---
+++ PIG/view/office/office.gd
@@ -0,0 +1,14 @@
+extends Node2D
+class_name Office
+
+## This script handes operations on the Office scene, such as viewing details
+## about the employees, scrolling the background, etc.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
