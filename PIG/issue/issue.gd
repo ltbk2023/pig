425c30ac3e5f514fe1c2911ea3db061d1a6368c4
pankogito <103581552+pankogito@users.noreply.github.com>
2023-06-02 21:16:40 +0200

    PIG-76 implementation of extended desricption hide/show button

--- PIG/issue/issue.gd
+++ PIG/issue/issue.gd
@@ -2,18 +2,18 @@ extends Node2D
 class_name Issue
 
 enum IssueType {FEATURE, BUG_ISSUE}
-# decription is used in Sumary
+# decription is used in Summary
 var type_decription = {
 	IssueType.FEATURE:"[color=BLACK]FEATURE[/color]",
-	IssueType.BUG_ISSUE:"[color=RED]BUG ISSUE[/color]"
+	IssueType.BUG_ISSUE:"[color=DARK_RED]BUG ISSUE[/color]"
 }
 
 enum IssueState {IN_BACKLOG, IN_PROGRESS, COMPLETED}
-# decription is used in Sumary
+# decription is used in Summary
 var  state_desriptions = {
-	IssueState.IN_BACKLOG:"[color=BLUE]in backlog[/color]",
-	IssueState.IN_PROGRESS: "[color=YELLOW]in backlog[/color]",
-	IssueState.COMPLETED: "[color=GREEN]completed[/color]",
+	IssueState.IN_BACKLOG:"[color=DARK_BLUE]in backlog[/color]",
+	IssueState.IN_PROGRESS: "[color=GOLD]in progres[/color]",
+	IssueState.COMPLETED: "[color=DARK_GREEN]completed[/color]",
 }
 
 @export var type:IssueType
@@ -21,12 +21,11 @@ var  state_desriptions = {
 @export_range(1,4) var difficulty = 1
 @export_range(1,3) var time = 1
 
-
 var __progress = 0
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	update_sumary()
+	update_summary()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -41,6 +40,11 @@ func set_visibility_od_exteded_desription(v):
 
 # update summary text
 #sumary include type, name, difficulty, time, state
-func update_sumary():
-	$Sumary.text =  type_decription[type]+" [color=BLACK]"+name+"  D "+str(difficulty)+" / T "+str(time)+"[/color] " + \
+func update_summary():
+	$Summary.text =  type_decription[type]+" [color=BLACK]"+name+"  D "+str(difficulty)+" / T "+str(time)+"[/color] " + \
 	state_desriptions[state] 
+
+
+# when button is realised toggle extended description visibility
+func _on_button_button_up():
+	set_visibility_od_exteded_desription(not $Extended.visible)
