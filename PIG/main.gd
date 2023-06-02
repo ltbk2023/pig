28dc30db6c4bb1d1b7016890b627d050ec8f112a
Blazej Tez <you@example.com>
2023-06-02 15:29:47 +0200

    PIG-79: Connect Quit Button

--- PIG/main.gd
+++ PIG/main.gd
@@ -10,9 +10,14 @@ func _ready():
 func _process(delta):
 	pass
 
-# Called when "Play" button is released. Hides menu and adds game scene to Main node
+# Called when "Play" button is released. Hides menu and adds game scene to Main node.
 func _on_play_button_up():
 	var game = load("res://game/game.tscn").instantiate()
 	add_child(game)
 	$Background.visible = false
 	$CanvasLayer.visible = false
+
+#TODO: implement check if game is saved before quitting the game.
+# Called when "Quit" button is released. Closes application.
+func _on_quit_button_up():
+	get_tree().quit()
