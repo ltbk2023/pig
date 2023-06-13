4219acdeea768bd54cb96694e8185dfe428e41fd
Blazej Tez <blatez@student.pg.edu.pl>
2023-06-13 13:16:20 +0200

    PIG-102, PIG-104: Autoconnect and forward signal in Office

--- PIG/view/office/office.gd
+++ PIG/view/office/office.gd
@@ -1,5 +1,7 @@
 extends Node2D
 class_name Office
+# signal containing owner, which should be employee, who is being assigned to the issue
+signal assign(owner)
 
 ## This script handes operations on the Office scene, such as viewing details
 ## about the employees, scrolling the background, etc.
@@ -13,8 +15,10 @@ func _process(delta):
 	pass
 	
 # Add an employee to the scene. Should be called from the Game script.
+# connect assign signal to _on_assign for added employee
 func add_employee(employee: Employee):
 	$Background/Employees.add_child(employee)
+	employee.assign.connect(_on_assign)
 
 # Remove an Employee from the Employees node. If delete_node is true, the Employee node will
 # also be deleted.
@@ -28,3 +32,7 @@ func remove_employee(employee: Employee, delete_node: bool):
 # Get a list of all Employees attached to the scene.
 func get_employees() -> Array[Node]:
 	return $Background/Employees.get_children()
+
+# sends signal with employee-owner to higher part of tree which should be Game
+func _on_assign(owner):
+	emit_signal("assign", owner)
