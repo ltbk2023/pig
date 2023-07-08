9f68e427a52eab22bf92ab10026251dd1a96427d
PLangowski <pawell092000@gmail.com>
2023-07-08 16:36:16 +0200

    PIG-128: Testing implementation

--- PIG/view/testing/testing.gd
+++ PIG/view/testing/testing.gd
@@ -1,6 +1,10 @@
 extends Node2D
 class_name Testing
 
+# Sent up the tree when a bug is found by testing. Should be caught by Game
+# which will add a random bug issue to the Backlog
+signal bug_found()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	update_quality_cards_text()
@@ -15,5 +19,14 @@ func update_quality_cards_text():
 	pass
 	
 # Update Result text to show the result of testing
-func update_result_text():
-	pass
+func update_result_text(amount: int):
+	$Background/Result.text = "Result:\nBugs: " + str(amount)
+	
+# Test [amount] quality cards. Update result visuals
+# TODO: add bug issue to backlog
+func test(amount: int):
+	var cards = $QualityDeck.check_cards(amount)
+	var bugs = cards[QualityDeck.QualityType.BUG]
+	update_result_text(amount)
+	emit_signal("bug_found")
+	
