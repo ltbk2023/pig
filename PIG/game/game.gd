bda2fcc09f9537d2da7b3614ec86f896df8bbf49
KKucht <krzysztof.k2001.01@wp.pl>
2023-09-24 21:51:18 +0200

    PIG-176: Load JSON from file

--- PIG/game/game.gd
+++ PIG/game/game.gd
@@ -290,3 +290,18 @@ func save_to_file(file_name):
 	var file = FileAccess.open(file_name, FileAccess.WRITE)
 	file.store_string(data)
 	file.close()
+
+# load JSON state of whole game from file
+func load_from_file(file_name: String) -> Dictionary:
+	var file = FileAccess.open(file_name, FileAccess.READ)
+	var content = file.get_as_text()
+	var json = JSON.new()
+	var error = json.parse(content)
+	if error == OK:
+		var data_received = json.data
+		data_received["error"] = null
+		return data_received
+	else:
+		print("JSON Parse Error: ", json.get_error_message(), " in ", content, \
+		" at line ", json.get_error_line())
+		return {"error": "JSON Parse Error"}
