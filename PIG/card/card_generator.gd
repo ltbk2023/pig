dc18446139c53128edeeb05631243e4ddb4235d2
pankogito <103581552+pankogito@users.noreply.github.com>
2023-11-11 19:43:05 +0100

    PIG-233: Card visual rework

--- PIG/card/card_generator.gd
+++ PIG/card/card_generator.gd
@@ -4,12 +4,16 @@ class_name CardGenerator
 @export var json_path: String = "story_cards.json"
 
 # Dictionary that holds story cards parsed from the file under :json_path
-var story_cards: Dictionary
+var story_cards: Array[CardDTO]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	story_cards = Utility.load_from_file(json_path)
-	story_cards.erase("error")
+	var story_cards_json = Utility.load_from_resource(json_path)
+	story_cards = []
+	for sc in story_cards_json:
+		var card = CardDTO.new()
+		card.load(sc)
+		story_cards.append(card)
 	randomize()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,6 +21,6 @@ func _process(delta):
 	pass
 
 # Return a random entry from the JSON File
-func generate_card() -> Dictionary:
-	var card_id: String = str(randi_range(0, story_cards.size() - 1))
-	return story_cards[card_id].duplicate(true)
+func generate_card() -> CardDTO:
+	var card_id = randi_range(0, story_cards.size() - 1)
+	return story_cards[card_id]
---
+++ PIG/card/dto/modify_stat_dto.gd
@@ -0,0 +1,29 @@
+extends Resource
+# stores information to create ModifyStat
+class_name ModifyStatDTO
+
+var employee
+var stat
+var stat_value
+var duration
+
+func load(list):
+	employee = list[0]
+	stat = list[1]
+	stat_value = list[2]
+	duration = list[3]
+	
+func create_and_fill_copy(reference)->ModifyStatDTO:
+	var dto = ModifyStatDTO.new()
+	dto.employee = reference[employee]
+	dto.stat = stat
+	dto.stat_value =stat_value
+	dto.duration =duration
+	return dto
+
+func create_modifier():
+	var s = Utility.str_to_stat(stat)
+	var modifier = preload("res://modifier/employee_stat_modifier.tscn").instantiate()
+	modifier.initialize(s, stat_value, duration)
+	modifier.attach_employee(employee)
+	return modifier
